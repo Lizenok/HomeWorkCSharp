0c6d1b523cf21d6252eb88bbce4e4f4a85b43c8e
Elizaveta <elizaveta.pyanova@gmail.com>
2022-11-19 00:48:37 +0100

    Добавлена задача 1

---
+++ task1_1/Program.cs
@@ -0,0 +1,21 @@
+﻿int numberA = new int();
+Console.WriteLine("Введите число");
+numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = new int();
+Console.WriteLine("Введите число");
+numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.Write("max = ");
+    Console.WriteLine(numberA);
+    Console.Write("min = ");
+    Console.WriteLine(numberB);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(numberB);
+    Console.Write("min = ");
+    Console.WriteLine(numberA);
+}
