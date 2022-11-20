1c9d263aeca91b49145857a0dd0696a63472763e
Elizaveta <elizaveta.pyanova@gmail.com>
2022-11-20 22:56:25 +0100

    Выполнила задание 2

---
+++ task1_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число А");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число C");
+int numberC = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+if(numberA > max) max = numberA;
+if(numberB > max) max = numberB;
+if(numberC > max) max = numberC;
+Console.WriteLine(max);
