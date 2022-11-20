5d5b1bd3c9c809e23a0391736fad21cd36d2fd51
Elizaveta <elizaveta.pyanova@gmail.com>
2022-11-20 23:46:20 +0100

    Выполнила задание 4

---
+++ task1_4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число");
+int Number = Convert.ToInt32(Console.ReadLine());
+int Start = 1;
+if (Number > 0)
+{   
+    Start = 2;
+    while (Start <= Number)
+    {
+        Console.Write(Start + " ");
+        Start = Start + 2;
+    }
+}
+else
+{
+    Start = 0;
+    while (Start >= Number)
+    {
+        Console.Write(Start + " ");
+        Start = Start - 2;
+    } 
+}
