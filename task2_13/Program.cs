7e82d64c268449ef5b31eac4566657828ec7e465
Elizaveta <elizaveta.pyanova@gmail.com>
2022-11-22 11:53:22 +0100

    Выполнила ДЗ семинара 2 зад.13

---
+++ task2_13/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет. 645 -> 5 78 -> третьей цифры нет 32679 -> 6*/
+Random randomGenerator = new Random();
+int initialNumber = randomGenerator.Next();
+if(initialNumber < 100)
+{
+    Console.WriteLine($"У числа {initialNumber} нет третьей цифры");
+}
+else
+{
+    int number = initialNumber;
+    while (number >= 1000)
+    {
+        number = number / 10;
+    }
+    int result = number % 10;
+    Console.WriteLine($"У числа {initialNumber} третья цифра {result}");
+}
