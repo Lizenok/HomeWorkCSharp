59835b1677ff2c2ee07ff8833d41e378c3204d07
Elizaveta <elizaveta.pyanova@gmail.com>
2022-11-22 09:52:12 +0100

    Выполнила ДЗ семинара 2 зад.15

---
+++ task2_15/Program.cs
@@ -0,0 +1,22 @@
+﻿int GetRandomNumberInRange(int start, int end)
+{
+    int result = 0;
+    result = new Random().Next(start, end + 1);
+    return result;
+}
+int randomNumber = GetRandomNumberInRange(1, 7);
+if(randomNumber == 6)
+{
+    Console.WriteLine($"День под номером {randomNumber} - да");
+}
+else
+{
+   if(randomNumber == 7)
+    {
+        Console.WriteLine($"День под номером {randomNumber} - да");
+    }
+    else
+    {
+        Console.WriteLine($"День под номером {randomNumber} - нет"); 
+    }
+}
