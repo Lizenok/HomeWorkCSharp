3c12f97680524e65cb7bb7f5094c4c24608b85ef
Elizaveta <elizaveta.pyanova@gmail.com>
2022-11-22 09:22:25 +0100

    Выполнила задание 10

---
+++ task2_10/Program.cs
@@ -0,0 +1,14 @@
+﻿int GetRandomNumberInRange(int start, int end)
+{
+    int result = 0;
+    result = new Random().Next(start, end + 1);
+    return result;
+}
+int GetToDigitNumber (int number)
+{
+    int second = (number % 100) / 10;
+    return (second);
+}
+int randomNumber = GetRandomNumberInRange(100, 999);
+int result = GetToDigitNumber (randomNumber);
+Console.WriteLine($"У числа {randomNumber} вторая цифра {result}");
