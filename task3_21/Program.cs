3a2167adc45f9e0e482f8514a2205bcb20280f4a
Elizaveta <elizaveta.pyanova@gmail.com>
2022-11-23 15:49:35 +0100

    HW семинар 3 зад. 21

---
+++ task3_21/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53*/
+
+int xa = GetNumberFromUser("Введите Xa:");
+int ya = GetNumberFromUser("Введите Ya:");
+int za = GetNumberFromUser("Введите Za:");
+int xb = GetNumberFromUser("Введите Xb:");
+int yb = GetNumberFromUser("Введите Yb:");
+int zb = GetNumberFromUser("Введите Zb:");
+
+CountDistance(xa, ya, za, xb, yb, zb);
+
+int GetNumberFromUser(string message)
+{
+    Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+void CountDistance(int xa, int ya, int za, int xb, int yb, int zb)
+{
+    double distance = Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2) + Math.Pow(zb - za, 2));
+    Console.WriteLine($"Расстояние между двумя точками в пространстве равно {distance}");
+}
