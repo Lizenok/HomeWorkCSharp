5d727354446e5d19a7abf0ac723565d0c3402598
Elizaveta <elizaveta.pyanova@gmail.com>
2022-11-25 11:18:38 +0100

    HW seminar 4 task 25

---
+++ task4_25/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+int getNumberFromUser(string message)
+{
+    Console.WriteLine(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int RiseIntoPower(int num, int power)
+{
+    int proizvod = 1;
+    for (int i = 1; i <= power; i++)
+    {
+        proizvod *= num;
+    }
+    return proizvod;
+}
+
+int userNumberA = getNumberFromUser("Введите число А");
+int userNumberB = getNumberFromUser("Введите число В");
+int proizvod = RiseIntoPower(userNumberA, userNumberB);
+Console.WriteLine($"Число {userNumberA} в степени {userNumberB} = {proizvod}");
