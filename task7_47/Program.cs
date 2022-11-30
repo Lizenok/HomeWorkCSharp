afaa797ab07782cab3a73c2f5c0278e972bce3e1
Elizaveta <elizaveta.pyanova@gmail.com>
2022-11-30 09:39:17 +0100

    HW seminar 7 task 47

---
+++ task7_47/Program.cs
@@ -0,0 +1,36 @@
+﻿/*Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9*/
+double[,] generate2DArray(int countRow, int countCol)
+{
+    double[,] array = new double[countRow,countCol];
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countCol; j++)
+        {
+            array[i,j] = new Random().NextDouble() * 100;
+        }
+    }
+    return array;
+}
+
+void print2dArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j <  array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество строк массива");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов массива");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] array = generate2DArray(m,n);
+print2dArray(array);
