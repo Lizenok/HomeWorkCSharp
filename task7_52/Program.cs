e3cc8f86505677e19c6b39d81010517d282f06a4
Elizaveta <elizaveta.pyanova@gmail.com>
2022-12-01 15:32:54 +0100

    HW seminar 7 task 52

---
+++ task7_52/Program.cs
@@ -0,0 +1,69 @@
+﻿/*Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
+
+int[,] generate2DArray(int countRow, int countCol, int deviation)
+{
+    int[,] array = new int[countRow, countCol];
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countCol; j++)
+        {
+            array[i, j] = new Random().Next(-deviation, deviation + 1);
+        }
+    }
+    return array;
+}
+
+void print2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[] arrayOfArithmeticalMean (int[,] array)
+{
+    double[] newArray = new double[array.GetLength(1)];
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+           sum += (double)array[i, j];
+        } 
+        newArray[j] = sum / array.GetLength(0);
+    }
+    return newArray;
+}
+
+void printArray(double[] array)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(";\t");
+        }
+    }
+    Console.ResetColor();
+}
+
+Console.WriteLine("Введите количество строк массива");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов массива");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] arrayOfElements = generate2DArray(m, n, 10);
+print2dArray(arrayOfElements);
+double[] arrayOfMeanValue = arrayOfArithmeticalMean(arrayOfElements);
+printArray(arrayOfMeanValue);
