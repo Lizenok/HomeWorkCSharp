fa7492400a5c6d354244f82a9317bc4b5af61641
Elizaveta <elizaveta.pyanova@gmail.com>
2022-12-05 10:36:16 +0100

    HW seminar 9 task 64

---
+++ task9_64/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+
+int StartIndex = getUserInt("Введите максимальный элемент ряда");
+printRange(StartIndex);
+
+
+int getUserInt(string message)
+{
+    Console.WriteLine(message);
+    int userInt = Convert.ToInt32(Console.ReadLine());
+    return userInt;
+}
+
+void printRange(int index)
+{
+    if (index != 0)
+    {
+        Console.Write(index);
+        if (index > 1)
+        {
+            Console.Write(", ");
+        }
+        printRange(index - 1);
+    }
+} 
